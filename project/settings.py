772907524277d046ed8a110da4fb8ae82b2aa143
Osmar Faria <osm.faria@gmail.com>
2023-01-07 10:40:21 -0800

    heroku config 2
    Signed-off-by: Osmar Faria <osm.faria@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -15,12 +15,15 @@ from pathlib import Path
 import django_on_heroku
 import dotenv
 import environ
+import dj_database_url
 
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
 dotenv.load_dotenv()
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
