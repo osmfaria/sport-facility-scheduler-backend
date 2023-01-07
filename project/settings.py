1322c1cb35137237dd962d10f3bcc034a2adafb0
Osmar Faria <osm.faria@gmail.com>
2023-01-07 10:49:46 -0800

    heroku config 2
    Signed-off-by: Osmar Faria <osm.faria@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -100,24 +100,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if os.environ.get("TEST"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-				"default": {
-						"ENGINE": "django.db.backends.postgresql",
-				        "NAME": env("POSTGRES_DB"),
-				        "USER": env("POSTGRES_USER"),
-				        "PASSWORD": env("POSTGRES_PASSWORD"),
-				        "HOST": "db",
-				        "PORT": 5432,
-				    }
-    }
+# if os.environ.get("TEST"):
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
+# else:
+#     DATABASES = {
+# 				"default": {
+# 						"ENGINE": "django.db.backends.postgresql",
+# 				        "NAME": env("POSTGRES_DB"),
+# 				        "USER": env("POSTGRES_USER"),
+# 				        "PASSWORD": env("POSTGRES_PASSWORD"),
+# 				        "HOST": "db",
+# 				        "PORT": 5432,
+# 				    }
+#     }
 
 
 # Password validation
