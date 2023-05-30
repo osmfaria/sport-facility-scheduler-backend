8f028e47e2434221fae8b618532c1f2c952f10d9
Osmar Faria <osm.faria@gmail.com>
2023-05-30 10:15:17 -0700

    add permission

--- facilities/permissions.py
+++ facilities/permissions.py
@@ -27,4 +27,7 @@ class IsTheOwnerOrAdmin(permissions.BasePermission):
 
         
     def has_object_permission(self, request: Request, view: View, facility: Facility) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
         return request.user.id == facility.user.id or request.user.is_superuser
