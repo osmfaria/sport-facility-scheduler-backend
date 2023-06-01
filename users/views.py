b6460f46746404ae6b806aeaef2e6b33672ac314
Osmar Faria <osm.faria@gmail.com>
2023-06-01 10:15:04 -0700

    fix: add user info on login

--- users/views.py
+++ users/views.py
@@ -10,71 +10,78 @@ from .token import AccountActivationTokenGenerator
 from django.http import HttpResponse, HttpResponseRedirect
 from users.models import User
 from users.permissions import IsOwnerOrAdmin
-from users.serializers import (LoginSerializer, UserDetailSerializer, UserSerializer)
+from users.serializers import LoginSerializer, UserDetailSerializer, UserSerializer
 from drf_spectacular.utils import extend_schema
 
 
-@extend_schema(tags=['User'])
-
+@extend_schema(tags=["User"])
 class ListUsersView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-@extend_schema(tags=['User'])
 
+
+@extend_schema(tags=["User"])
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        
+
         user = User.objects.get(id=serializer.data["id"])
-        
+
         sendActivateEmail(request, user=user, recipient=[user.email])
 
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
 
-@extend_schema(description='Confirmation email must be activated before first login')
-
+@extend_schema(description="Confirmation email must be activated before first login")
 class LoginView(APIView):
     # added for auto generated documentation only
     serializer_class = LoginSerializer
     queryset = User.objects.all()
     # -------------------------------------------
 
-    @extend_schema(tags=['User'])
-
+    @extend_schema(tags=["User"])
     def post(self, request: Request) -> Response:
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = authenticate(**serializer.validated_data)
+        user: User = authenticate(**serializer.validated_data)
 
         if user:
-            token,_ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key, "user": user}, status.HTTP_200_OK)
+            token, _ = Token.objects.get_or_create(user=user)
+            username = user.username.capitalize()
+            return Response(
+                {
+                    "token": token.key,
+                    "username": username,
+                    "id": user.id,
+                    "is_owner": user.is_owner,
+                },
+                status.HTTP_200_OK,
+            )
 
-        return Response({"detail": "invalid username or password"}, status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"detail": "invalid username or password"}, status.HTTP_400_BAD_REQUEST
+        )
 
 
-@extend_schema(tags=['User'])
-@extend_schema(description='Must be the account owner or admin', methods=["DELETE", "PATCH", "PUT"])
-
+@extend_schema(tags=["User"])
+@extend_schema(
+    description="Must be the account owner or admin", methods=["DELETE", "PATCH", "PUT"]
+)
 class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsOwnerOrAdmin]
-    
+
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
-    lookup_url_kwarg = 'user_id'
-
-
+    lookup_url_kwarg = "user_id"
 
 
 def activate(request, uidb64, token):
@@ -83,13 +90,13 @@ def activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-    
+
         return HttpResponseRedirect("https://www.google.com/")
 
     else:
